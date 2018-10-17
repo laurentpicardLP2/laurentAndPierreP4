b6af6f602e8a3e6fdc27037682446ab37c5399a0
GORCE Pierre <looupsing@gmail.com>
2018-10-17 12:51:51 +0200

    add player.java

---
+++ Player.java
@@ -0,0 +1,20 @@
+/**
+ * Player
+ */
+public class Player {
+
+    private String name;
+    private boolean isVictory;
+    Scanner input; 
+
+    public Player() {
+        input = new Scanner(System.in);
+    }
+
+    public void play(Grid grid) {
+        System.out.print("Choissisez une colonne: ");
+        int colNum = input.nextInt();
+        grid.addToken(colNum);
+
+    }
+}
--- Puissance4.java
+++ Puissance4.java
@@ -2,4 +2,6 @@ public class Puissance4 {
   public static void main(String[] args){
     System.out.println("Puissance4");
   }
+
+  
 }
