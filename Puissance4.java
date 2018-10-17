6c893b142148a33ccd89b3359d48359e473626a5
GORCE Pierre <looupsing@gmail.com>
2018-10-17 12:57:26 +0200

    fix bug before lunch

--- Puissance4.java
+++ Puissance4.java
@@ -4,12 +4,12 @@ public class Puissance4 {
   public static void main(String[] args){
     int nbCoups = 0;
     //int firstPlayer = Math.floor(Math.random()*2);
-    Grille grid = new Grid(7, 6);
+    Grid grid = new Grid(7, 6);
 
     Player player1 = new Player();
     Player player2 = new Player();
     player1.play(grid);
   }
 
-  
+
 }
